34ed29fd00abf8216a513ea556153bffe39ea553
Hubert Wajda <hubert.wajda03@gmail.com>
2024-09-27 01:08:49 +0200

    setup docker

---
+++ backend/src/test/java/com/TTT_backend/TttBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TTT_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TttBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
