af1e366c4f318da4e6386b835efef840e173ec4c
Hubert Wajda <hubert.wajda03@gmail.com>
2025-02-03 10:58:26 +0100

    correct overlay

--- frontend/src/components/shared/Overlay.jsx
+++ frontend/src/components/shared/Overlay.jsx
@@ -1,33 +1,41 @@
 import React from "react";
 import {AnimatePresence, motion} from "framer-motion";
+import {createPortal} from "react-dom";
 
 function Overlay({ isDisplayed, setDisplay, children }) {
   return (
-    <AnimatePresence>
-      {isDisplayed && (
-        <motion.div className="overlay" exit={{opacity: 0 ,transition: {duration: 0.09} }}>
-          <motion.div
-              className="container align-middle my-center "
-              initial={{scale: 0, x: -10} }
-              animate={{scale: 1, x: 0}}
-              transition={{duration: 0.09}}
-              exit={{scale: 0 ,transition: {duration: 0.09} }}
-          >
-              <div className="d-flex justify-content-center bg-white  bg-white rounded px-5 py-4">
-                {children}
-              </div>
-            <div className="d-flex justify-content-center">
-                <div
-                  className="btn btn-danger m-4"
-                  onClick={(e) => setDisplay(false)}
-                >
-                  Cancel
-                </div>
-            </div>
-          </motion.div>
-        </motion.div>
-      )}
-    </AnimatePresence>
+      <>
+        {createPortal(
+            <AnimatePresence>
+              {isDisplayed && (
+                  <motion.div className="overlay" exit={{opacity: 0 ,transition: {duration: 0.09} }}>
+                    <motion.div
+                        className="container align-middle my-center "
+                        initial={{scale: 0, x: -10} }
+                        animate={{scale: 1, x: 0}}
+                        transition={{duration: 0.09}}
+                        exit={{scale: 0 ,transition: {duration: 0.09} }}
+                    >
+                      <div className="d-flex justify-content-center bg-white  bg-white rounded px-5 py-4">
+                        {children}
+                      </div>
+                      <div className="d-flex justify-content-center">
+                        <div
+                            className="btn btn-danger m-4"
+                            onClick={(e) => setDisplay(false)}
+                        >
+                          Cancel
+                        </div>
+                      </div>
+                    </motion.div>
+                  </motion.div>
+              )}
+            </AnimatePresence>,
+            document.getElementById("root")
+        )
+        }
+
+      </>
   );
 }
 
