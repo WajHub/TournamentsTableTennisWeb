febbdc43b11cce03fe857b454ccf71514f242ebf
Hubert Wajda <hubert.wajda03@gmail.com>
2025-03-08 15:07:48 +0100

    prod version

--- backend/src/main/java/com/ttt/backend/configs/SecurityConfiguration.java
+++ backend/src/main/java/com/ttt/backend/configs/SecurityConfiguration.java
@@ -68,7 +68,7 @@ public class SecurityConfiguration {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
