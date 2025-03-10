27412a2bf0e3f929605234ba603f3f5a8a2cad8f
Hubert Wajda <hubert.wajda03@gmail.com>
2025-03-10 22:33:04 +0100

    next try to deploy...

---
+++ backend/src/main/resources/db/migration/V2__insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO users (email, fullname, password, role, is_active)
+VALUES ('hubwaj7@gmail.com', 'WajHub-Admin', '$2a$12$PQYIuKIjRNPTN5inJNfwOO0sRekUaoJwJpw.utBkk1mMasRdet9.e', 0, true);
+INSERT INTO categories (name, type, age_limit, gender)
+VALUES ('Senior - man', 'SINGLE', 999, 'MAN');
+INSERT INTO categories (name, type, age_limit, gender)
+VALUES ('Senior - woman', 'SINGLE', 999, 'WOMAN');
+INSERT INTO categories (name, type, age_limit, gender)
+VALUES ('U19 - man', 'SINGLE', 19, 'MAN');
+INSERT INTO categories (name, type, age_limit, gender)
+VALUES ('U19 - woman', 'SINGLE', 19, 'WOMAN');
+INSERT INTO categories (name, type, age_limit, gender)
+VALUES ('U15 - man', 'SINGLE', 15, 'MAN');
+INSERT INTO categories (name, type, age_limit, gender)
+VALUES ('U15 - woman', 'SINGLE', 15, 'WOMAN');
