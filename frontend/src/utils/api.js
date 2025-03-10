847ac990074b88bab992ecd7beb6917e4294bf71
Hubert Wajda <hubert.wajda03@gmail.com>
2025-03-10 01:23:38 +0100

    delete testing logs

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-const apiUrl = import.meta.env.VITE_API_URL;
-console.log("TEEEEST", import.meta.env)
+const apiUrl = import.meta.env.VITE_API_URL;ć
 
 export const loadEvent = async (id) => {
   try {
